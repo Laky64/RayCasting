6b41df45becf7ee53e2ec97e0bbc0d6e20111e66
Laky <59400426+Laky64@users.noreply.github.com>
2022-05-03 11:29:10 +0200

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "../include/mainLoop.h"
+
+int main()
+{
+    // init GameEngine
+    MainLoop mainLoop;
+
+    // Game loop
+    while (mainLoop.running())
+    {
+        // Update
+        mainLoop.update();
+
+        // Render
+        mainLoop.render();
+    }
+
+    return 0;
+}
