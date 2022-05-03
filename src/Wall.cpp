86bc1908627e15f2e02ee9276a095990fd1f16b3
Laky <59400426+Laky64@users.noreply.github.com>
2022-05-03 11:30:13 +0200

    Add files via upload

---
+++ src/Wall.cpp
@@ -0,0 +1,21 @@
+#include "Wall.h"
+
+Wall::Wall(sf::Vector2f start, sf::Vector2f end)
+{
+	this->start = start;
+	this->end = end;
+	wall[0].color = sf::Color::Green;
+	wall[1].color = sf::Color::Green;
+
+	wall[0].position = start;
+	wall[1].position = end;
+}
+
+Wall::~Wall()
+{
+}
+
+void Wall::render(sf::RenderWindow* window)
+{
+	window->draw(wall, 2, sf::Lines);
+}
