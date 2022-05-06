c9b3007322ba1661b13f6fcb5aec53e66f35d4ad
Laky <59400426+Laky64@users.noreply.github.com>
2022-05-06 13:10:07 +0200

    Add files via upload

---
+++ include/MainLoop.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+#include "RayCaster.h"
+
+
+struct KeyMapWASD {
+	bool W;
+	bool A;
+	bool S;
+	bool D;
+};
+
+struct KeyMap {
+	bool Left;
+	bool Right;
+};
+
+class MainLoop
+{
+public:
+	MainLoop();
+	virtual ~MainLoop();
+
+	//Called by main every frame
+	void update();
+	void render();
+
+	// Accessors
+	const bool running() const;
+
+private:
+	//-Window-//
+	sf::RenderWindow* window;
+
+	//-Event-//
+	sf::Event event;
+
+
+	//-initilize-//
+	void initVariables();
+	void initWindow();
+	void initGameObjects();
+	void initGUI();
+
+
+	//-Events-//
+	void pollEvents();
+	void updateGameObjects();
+	void updateGUI();
+
+
+	//--Private Variables --//
+
+	//-inputs-//
+	sf::Vector2i MousePosition;
+	bool leftMouseButton;
+
+
+
+	KeyMapWASD keyMapWASD;
+	KeyMap keyMap;
+
+	//-timing-fps-//
+	float fps;
+	sf::Clock clock;
+	sf::Time previousTime;
+	sf::Time currentTime;
+
+
+	//Elements
+
+	RayCaster rayCaster;
+
+
+
+
+	//movement
+	sf::Vector2f PlayerPos;
+
+	float lookDir;
+
+	float speed;
+
+	//Background
+	sf::RectangleShape top;
+	sf::RectangleShape bottom;
+
+	//Walls
+	std::vector<Wall> wallList;
+
+};
---
+++ include/Wall.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+class Wall
+{
+public:
+	Wall(sf::Vector2f start, sf::Vector2f end);
+	virtual ~Wall();
+	sf::Vector2f start;
+	sf::Vector2f end;
+	sf::Vertex wall[2];
+	void render(sf::RenderWindow* window);
+};
+
